c185ddeae110bdcff83367db0af37a3ed38244cf
Ajdgreat <anjorin.kingdavid@stu.cu.edu.ng>
2025-04-11 22:55:26 +0100

    last bug fix

--- backend_folder/database.py
+++ backend_folder/database.py
@@ -3,4 +3,4 @@ import os
 
 # Connect to PostgreSQL using environment variable
 def get_connection():
-    return psycopg2.connect(os.environ("DATABASE"))
+    return psycopg2.connect(os.environ["DATABASE"])
